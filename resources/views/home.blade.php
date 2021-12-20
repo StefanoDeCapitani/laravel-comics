f624ff1412c2df61b56b8c7aa03d936710f1d5d0
Stefano De Capitani <stefano.deca@live.com>
2021-12-20 14:58:52 +0100

    Project setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('page_title', "Homepage - DC Comics")
+
+@section('content')
+    <h1>Main content goes here...</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,1 @@
+<h1>Header content goes here...</h1>
